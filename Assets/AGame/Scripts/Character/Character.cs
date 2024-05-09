56ba20ff5079a2d6d5711d666c8a925c089b1a8e
NNT.Koala <105761601+NNTKOALA@users.noreply.github.com>
2024-05-09 16:12:53 +0700

    feats: characters animation

--- Assets/AGame/Scripts/Character/Character.cs
+++ Assets/AGame/Scripts/Character/Character.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField] float health = 0f;
-    [SerializeField] float damage = 0f;
-    [SerializeField] float range = 0f;
-    [SerializeField] float level = 0f;
+    [SerializeField] public Animator anim;
+    [SerializeField] protected float health = 0f;
+    [SerializeField] protected float damage = 0f;
+    [SerializeField] protected float range = 0f;
+    [SerializeField] protected float level = 0f;
     [SerializeField] CharType charType;
+
+    [SerializeField] protected Collider[] targetInRange;
+
+    protected string currentAnim = "";
+    public bool isDead { get; set; } = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isDead = false;
     }
 
     // Update is called once per frame
@@ -22,17 +28,101 @@ public class Character : MonoBehaviour
         
     }
 
+    protected void ChangeAnim(string animName)
+    {
+        if (currentAnim != animName)
+        {
+            if (!string.IsNullOrEmpty(animName))
+            {
+                Debug.Log("play anim " + animName);
+
+                anim.ResetTrigger(currentAnim);
+                currentAnim = animName;
+                anim.SetTrigger(currentAnim);
+            }
+            else
+            {
+                currentAnim = "";
+            }
+        }
+    }
+
     public void TakeDamage()
     {
         health -= damage;
         if (health <= 0)
         {
             Debug.Log("Die");
+            OnDead();
         }
     }
 
     public void MergeCharacter()
     {
 
+    }
+
+/*    public Character FindClosetEnemy()
+    {
+        targetInRange = Physics.OverlapSphere(transform.position, range, characterMask);
+        Collider nearestEnemy = null;
+        if (targetInRange.Length == 0)
+        {
+            return null;
+        }
+        else
+        {
+            float minimumDistance = Mathf.Infinity;
+
+            foreach (Collider enemy in targetInRange)
+            {
+                if (enemy == this.GetComponent<Collider>())
+                {
+                    continue;
+                }
+                if (enemy.GetComponent<Character>().isDead)
+                {
+                    continue;
+                }
+                float distance = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distance < minimumDistance)
+                {
+                    minimumDistance = distance;
+                    nearestEnemy = enemy;
+                }
+            }
+            if (nearestEnemy != null)
+            {
+                return nearestEnemy.GetComponent<Character>();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }*/
+
+    public void LookAtTarget(Vector3 target)
+    {
+        transform.LookAt(target);
+    }
+
+    public void OnMove()
+    {
+        ChangeAnim("move");
+        Debug.Log("Move");
+    }
+
+    public void OnAttack()
+    {
+        ChangeAnim("attack");
+        Debug.Log("Attack");
+    }
+
+    public void OnDead()
+    {
+        isDead = true;
+        ChangeAnim("die");
+        Debug.Log("Die");
     }
 }
