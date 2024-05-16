77287c5d20cfc2015355eedf7275839e65831620
NNT.Koala <105761601+NNTKOALA@users.noreply.github.com>
2024-05-16 11:52:02 +0700

    feats: character move and attack, character health bar

--- Assets/AGame/Scripts/Character/Character.cs
+++ Assets/AGame/Scripts/Character/Character.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.TextCore.Text;
 
 public class Character : MonoBehaviour
@@ -9,34 +10,62 @@ public class Character : MonoBehaviour
     [SerializeField] LayerMask playerTile;
     [SerializeField] protected int health;
     [SerializeField] protected int damage;
-    [SerializeField] protected int range;
     [SerializeField] CharLevel charLevel;
     [SerializeField] CharType charType;
+    [SerializeField] float chaseRange = 5f;
+    [SerializeField] float turnSpeed = 5f;
 
-    [SerializeField] protected Collider[] targetInRange;
+    public Transform target;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+    //Transform target;
 
     protected string currentAnim = "";
     public bool isDead { get; set; } = false;
 
 /*    protected int maxHealth = 100;
     protected int currentHealth;
-
-    public HealthBar healthBar;*/
+    public GameObject healthBarPrefab;
+    private HealthBar healthBar;*/
 
     // Start is called before the first frame update
     protected virtual void Start()
     {
         isDead = false;
         health = 100;
+        navMeshAgent = GetComponent<NavMeshAgent>();
         CheckOnTile();
+
 /*        currentHealth = maxHealth;
+        GameObject hb = Instantiate(healthBarPrefab, transform);
+        hb.transform.localPosition = new Vector3(0, 2, 0);
+        healthBar = hb.GetComponent<HealthBar>();
         healthBar.SetMaxHealth(maxHealth);*/
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
+        if (target != null)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
 
+/*        if (isDead)
+        {
+            enabled = false;
+        }
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }*/
     }
 
     protected void ChangeAnim(string animName)
@@ -60,70 +89,14 @@ public class Character : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        isProvoked = true;
         health -= damage;
-        //healthBar.SetHealth(health);
+        //healthBar.SetHealth(currentHealth);
         if (health <= 0)
         {
             Debug.Log("Die");
             OnDead();
         }
-    }
-
-/*    public Character FindClosetEnemy()
-    {
-        targetInRange = Physics.OverlapSphere(transform.position, range, characterMask);
-        Collider nearestEnemy = null;
-        if (targetInRange.Length == 0)
-        {
-            return null;
-        }
-        else
-        {
-            float minimumDistance = Mathf.Infinity;
-
-            foreach (Collider enemy in targetInRange)
-            {
-                if (enemy == this.GetComponent<Collider>())
-                {
-                    continue;
-                }
-                if (enemy.GetComponent<Character>().isDead)
-                {
-                    continue;
-                }
-                float distance = Vector3.Distance(transform.position, enemy.transform.position);
-                if (distance < minimumDistance)
-                {
-                    minimumDistance = distance;
-                    nearestEnemy = enemy;
-                }
-            }
-            if (nearestEnemy != null)
-            {
-                return nearestEnemy.GetComponent<Character>();
-            }
-            else
-            {
-                return null;
-            }
-        }
-    }*/
-
-    public void LookAtTarget(Vector3 target)
-    {
-        transform.LookAt(target);
-    }
-
-    public void OnMove()
-    {
-        ChangeAnim("move");
-        Debug.Log("Move");
-    }
-
-    public void OnAttack()
-    {
-        ChangeAnim("attack");
-        Debug.Log("Attack");
     }
 
     public void OnDead()
@@ -155,5 +128,43 @@ public class Character : MonoBehaviour
         RaycastHit hit;
 
         return Physics.Raycast(transform.position + Vector3.up * 0.2f, Vector3.down * 2f, out hit, 0.4f, playerTile);
+    }
+
+    private void EngageTarget()
+    {
+        FaceTarget();
+
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
